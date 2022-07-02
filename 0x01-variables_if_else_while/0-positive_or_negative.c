e90c03eef7234bb86c5f0d50cc76ed94180ac547
amisieric6 <amisieric6@gmail.com>
2022-07-02 10:20:49 +0000

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
-*main => assign a random number to the variable n each time it is executed
-*and prints out based on condition
-*Return : always 0
+* main => assign a random number to the variable n each time it is executed
+* and prints out based on condition
+* Return : always 0
 */
 int main(void)
+	return (void);
 {
-int n;
+	int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
