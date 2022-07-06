4ad72460cf761d9979bf6fd0e593a1e28431b192
amisieric6 <amisieric6@gmail.com>
2022-07-06 06:04:20 +0000

    add 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main ->print_alphabet_*10
+*Return:Always return 0
+*/
+void print_alphabet_x10(void)
+{
+	int j;
+	for (j = '10*a'; j <= '10*z'; j++)
+
+	{
+	_putchar(j);
+	}
+	_putchar('\n');
+
+	return (0);
+}
